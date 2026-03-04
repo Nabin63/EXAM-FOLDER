8e630d0beebdcb44d28953871a19fc453d3a4613
Nabin63 <nabinsamanta14@gmail.com>
2026-03-04 12:56:40 +0530

    Update question.js

--- question.js
+++ question.js
@@ -1,27 +1,35 @@
+```javascript
 const questions = [
+
 {
-question: "He ___ a student.",
-options: ["is","are","was","were"],
-answer: "is"
+question:"He ___ a student.",
+options:["is","are","was","were"],
+answer:"is"
 },
+
 {
-question: "They ___ playing football.",
-options: ["is","are","was","were"],
-answer: "are"
+question:"They ___ playing football.",
+options:["is","are","was","were"],
+answer:"are"
 },
+
 {
-question: "She ___ absent yesterday.",
-options: ["is","are","was","were"],
-answer: "was"
+question:"She ___ absent yesterday.",
+options:["is","are","was","were"],
+answer:"was"
 },
+
 {
-question: "We ___ happy today.",
-options: ["is","are","was","were"],
-answer: "are"
+question:"We ___ happy today.",
+options:["is","are","was","were"],
+answer:"are"
 },
+
 {
-question: "I ___ very tired yesterday.",
-options: ["is","are","was","were"],
-answer: "was"
+question:"I ___ very tired yesterday.",
+options:["is","are","was","were"],
+answer:"was"
 }
+
 ];
+```
