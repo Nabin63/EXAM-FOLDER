f04695bfc6a1fd31afacb6dba287a6d362ae3da1
Nabin63 <nabinsamanta14@gmail.com>
2026-03-04 11:57:41 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,27 @@
+const questions = [
+{
+question: "He ___ a student.",
+options: ["is","are","was","were"],
+answer: "is"
+},
+{
+question: "They ___ playing football.",
+options: ["is","are","was","were"],
+answer: "are"
+},
+{
+question: "She ___ absent yesterday.",
+options: ["is","are","was","were"],
+answer: "was"
+},
+{
+question: "We ___ happy today.",
+options: ["is","are","was","were"],
+answer: "are"
+},
+{
+question: "I ___ very tired yesterday.",
+options: ["is","are","was","were"],
+answer: "was"
+}
+];
