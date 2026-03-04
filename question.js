e6e8a7626b2578e313322b0b21a97a502085417a
Nabin63 <nabinsamanta14@gmail.com>
2026-03-04 21:05:03 +0530

    Update question.js

--- question.js
+++ question.js
@@ -1,7 +1,7 @@
 const questions = [
 
 {
-question:"1. He ___ a student.",
+question:"1. He ___ a teacher.",
 a:"is",
 b:"are",
 c:"was",
@@ -10,7 +10,7 @@ correct:"a"
 },
 
 {
-question:"2. They ___ playing cricket.",
+question:"2. They ___ playing football.",
 a:"is",
 b:"are",
 c:"was",
@@ -28,34 +28,424 @@ correct:"c"
 },
 
 {
-question:"4. We ___ in the park yesterday.",
+question:"4. We ___ in school yesterday.",
 a:"is",
 b:"are",
 c:"was",
 d:"were",
 correct:"d"
+},
+
+{
+question:"5. I ___ ready now.",
+a:"is",
+b:"am",
+c:"are",
+d:"were",
+correct:"b"
 }
+{
+question:"6. They ___ playing football.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
 
+{
+question:"7. He ___ my brother.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"8. We ___ good friends.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"9. She ___ absent yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"c"
+},
+
+{
+question:"10. The boys ___ in the park.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"11. The teacher ___ in the class.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"12. They ___ happy yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"13. The dog ___ barking.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"14. We ___ late yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"15. The girl ___ singing.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"16. The students ___ ready.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"17. He ___ busy yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"c"
+},
+
+{
+question:"18. The birds ___ flying.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"19. She ___ my sister.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"20. They ___ tired yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"21. The flowers ___ beautiful.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"22. The baby ___ sleeping.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"23. The players ___ ready.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"24. He ___ my best friend.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"25. We ___ at home yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"26. The bus ___ late.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"27. The children ___ playing.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"28. I ___ very tired yesterday.",
+a:"is",
+b:"am",
+c:"are",
+d:"were",
+correct:"b"
+},
+
+{
+question:"29. They ___ in the classroom.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"30. She ___ very kind.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"31. The boys ___ happy.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"32. He ___ sick yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"c"
+},
+
+{
+question:"33. The cows ___ in the field.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"34. We ___ students.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"35. The man ___ my uncle.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"36. They ___ very hungry.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"37. The teacher ___ absent yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"c"
+},
+
+{
+question:"38. The girls ___ dancing.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"39. He ___ a good player.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"40. We ___ ready for the exam.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"41. The dog ___ very hungry.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"42. The boys ___ playing cricket.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"43. She ___ very tired yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"c"
+},
+
+{
+question:"44. The birds ___ in the sky.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"45. He ___ a teacher.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"46. They ___ late yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"47. The children ___ happy.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"b"
+},
+
+{
+question:"48. The girl ___ very smart.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+},
+
+{
+question:"49. We ___ in the playground yesterday.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"d"
+},
+
+{
+question:"50. The teacher ___ very kind.",
+a:"is",
+b:"are",
+c:"was",
+d:"were",
+correct:"a"
+}
 ];
-
-const quiz = document.getElementById("quiz");
-
-questions.forEach((q,i)=>{
-
-const div = document.createElement("div");
-
-div.innerHTML = `
-<p><b>${q.question}</b></p>
-
-<label><input type="radio" name="q${i}" value="a"> ${q.a}</label><br>
-
-<label><input type="radio" name="q${i}" value="b"> ${q.b}</label><br>
-
-<label><input type="radio" name="q${i}" value="c"> ${q.c}</label><br>
-
-<label><input type="radio" name="q${i}" value="d"> ${q.d}</label><br><br>
-`;
-
-quiz.appendChild(div);
-
-});
